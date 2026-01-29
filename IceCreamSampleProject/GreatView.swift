ccd8798f547f44f492f27fcb951b2c90a36fac56
Maria Reyna <maria.reyna2244@gmail.com>
2026-01-29 13:55:17 -0500

    I changed the file greatview to goodbye world

---
+++ IceCreamSampleProject/GreatView.swift
@@ -0,0 +1,18 @@
+//
+//  GreatView.swift
+//  IceCreamSampleProject
+//
+//  Created by Maria Reyna on 1/29/26.
+//
+
+import SwiftUI
+
+struct GreatView: View {
+    var body: some View {
+        Text("Goodbye, World!")
+    }
+}
+
+#Preview {
+    GreatView()
+}
